63bb270c2409ea0c0a5387390f2065581c021b83
Svenska Bio <Svenska Bio@SvenskaBio>
2017-10-04 14:12:18 +0200

    SwitchOnCard and Deck is cleared

--- group4blackjack/src/group4blackjack/Card.java
+++ group4blackjack/src/group4blackjack/Card.java
@@ -1,37 +1,56 @@
 package group4blackjack;
-//sad
-/****
- * 
- * The best version
- * @param <Suits>
- * 
- */
 
-public class Card<Suits> {
-	private Suits cardSuit;
-	private int cardNum;
-	private int[] numInt = {2,3,4, 5, 6, 7, 8, 9, 10,11};
+public class Card {
 
-	public Card (Suits stype, int snum) {
-		this.cardSuit = stype;
-		if (snum>=1 && snum <=13)
-			this.cardNum = snum;
-		else {
-			System.err.println(snum+" is not a valid card number\n");
-			System.exit(1);
+	int value;
+	String valueShown;
+
+	public Card(int value) {
+		super();
+		this.value = value;
+		switch (value) {
+		case 2:
+			valueShown="2";
+			break;
+		case 3:
+			valueShown="3";
+			break;
+		case 4:
+			valueShown="4";
+			break;
+		case 5:
+			valueShown="5";
+			break;
+		case 6:
+			valueShown="6";
+			break;
+		case 7:
+			valueShown="7";
+			break;
+		case 8:
+			valueShown="8";
+			break;
+		case 9:
+			valueShown="9";
+			break;
+		case 10:
+			valueShown="10";
+			break;
+		case 11:
+			valueShown="11";
+			break;
+
+		default:
+			break;
 		}
 	}
- 
-public int getCardNumber(){
-	
-	return this.cardNum;
-}
 
-public String toString(){
-	
-	return this.numInt[this.cardNum - 1]+" of "+this.cardSuit.toString();
-}
+	public int getValue() {
+		return value;
+	}
 
-
+	public void setValue(int value) {
+		this.value = value;
+	}
 
 }
--- group4blackjack/src/group4blackjack/Deck.java
+++ group4blackjack/src/group4blackjack/Deck.java
@@ -1,24 +1,16 @@
 package group4blackjack;
 
+import java.util.Arrays;
+
 /****
  * 
  * The best version
  * 
  */
-
-public class Deck {
+public class Deck{
 	
-
-	void Deckgenerator()
-	{
-	       Deck cards[][]=new Deck[4][13];
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 13; j++) {
-				cards[i][j]=new Deck(i,j);
-				
-			}
-		}
 	
-	}
 
 }
+
+	
