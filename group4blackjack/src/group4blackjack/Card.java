af2b67e2226350f42858e28aa0e7fe45e857e4d5
Svenska Bio <Svenska Bio@SvenskaBio>
2017-10-04 15:21:30 +0200

    getters, setters and constructor for card

--- group4blackjack/src/group4blackjack/Card.java
+++ group4blackjack/src/group4blackjack/Card.java
@@ -5,6 +5,26 @@ public class Card {
 	private int value;
 	private int suit;
 	
+	public Card(int value, int suit) {
+		super();
+		this.value = value;
+		this.suit = suit;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public int getSuit() {
+		return suit;
+	}
+	public void setSuit(int suit) {
+		this.suit = suit;
+	}
+	
+	
+	
 
 	
 
