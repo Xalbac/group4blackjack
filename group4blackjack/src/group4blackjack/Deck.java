6b808b7243b533d830397d249a6565b6e673f2cc
Svenska Bio <Svenska Bio@SvenskaBio>
2017-10-04 13:39:49 +0200

    Deck with for loop

--- group4blackjack/src/group4blackjack/Deck.java
+++ group4blackjack/src/group4blackjack/Deck.java
@@ -11,6 +11,13 @@ public class Deck {
 
 	void Deckgenerator()
 	{
+	       Deck cards[][]=new Deck[4][13];
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 13; j++) {
+				cards[i][j]=new Deck(i,j);
+				
+			}
+		}
 	
 	}
 
