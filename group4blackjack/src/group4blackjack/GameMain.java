6dfe5020c2aa15f30efbd48d7a5bf96ca5412635
Damian Wozniak <damian.pwozniak@gmail.com>
2017-10-04 11:32:18 +0200

    ds

--- group4blackjack/src/group4blackjack/GameMain.java
+++ group4blackjack/src/group4blackjack/GameMain.java
@@ -5,6 +5,11 @@ public class GameMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		/*If we have money: Do while (money > 0)
+		{
+			
+		}*/
 	}
 
 }
