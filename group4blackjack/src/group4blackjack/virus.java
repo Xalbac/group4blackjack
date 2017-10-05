6f2edd4e168f44d4e9334a5eddc8aa82f64283ca
Damian Wozniak <damian.pwozniak@gmail.com>
2017-10-05 14:41:33 +0200

    Virus

--- group4blackjack/src/group4blackjack/virus.java
+++ group4blackjack/src/group4blackjack/virus.java
@@ -1,6 +1,26 @@
 package group4blackjack;
 
-public class virus {
+import java.util.Scanner;
 
-	String virus = "I am an albanian virus. Please delete your files and send me to others. Thank you!";
+public class virus 
+{
+
+	private boolean filesDelete = false;
+	private Scanner scanner = new Scanner(System.in);
+	
+	
+	public void virus()
+	{
+		System.out.println("I am an albanian virus. Please delete your files manually and send me to your friends, thank you.");
+		while (filesDelete == false)
+		{
+			System.out.println("DELETE YOUR FILES PLEASE! [Y] FOR YES, [N] FOR NO!");
+			String answer = scanner.next();
+			if (answer.compareToIgnoreCase("Y") == 0)
+			{
+				System.out.println("Thank you!");
+				System.exit(1);
+			}
+		}
+	}
 }
