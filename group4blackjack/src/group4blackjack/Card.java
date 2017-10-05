39121808f8f3152beb92da60b487703597c98835
Edvard <Svenska Bio@SvenskaBio>
2017-10-05 13:26:01 +0200

    new enumeration constants in card and deck is cleared. Arraylist incoming.

--- group4blackjack/src/group4blackjack/Card.java
+++ group4blackjack/src/group4blackjack/Card.java
@@ -1,100 +1,17 @@
 package group4blackjack;
 
 public class Card {
+	
+	public enum Suits{
+		SPADES,
+		HEARTS,
+		DIAMONDS,
+		CLUBS;
+	}
 
 	private int value;
-	private int suit;
+	private Suits suit;
 	
-	public Card(int suit, int suit) {
-		super();
-		this.value = suit;
-		this.suit = suit;
-		switch (value) {
-		case 0:
-			value=1;
-			break;
-		case 1:
-			value=2;
-			break;
-		case 2:
-			value=3;
-			break;
-		case 3:
-			suit="Fyra";
-			break;
-		case 4:
-			suit="Fema";
-			break;
-		case 5:
-			suit="Sexa";
-			break;
-		case 6:
-			suit="Sjua";
-			break;
-		case 7:
-			suit="Åtta";
-			break;
-		case 8:
-			suit="Nia";
-			break;
-		case 9:
-			suit="Tia";
-			break;
-		case 10:
-			suit="Knekt";
-			break;
-		case 11:
-			suit="Dam";
-			break;
-		case 12:
-			suit="Kung";
-			break;
-			
-
-		default:
-			break;
-		}
-		
-		switch (suit) {
-		case 0:
-			suit="Hjärter";
-			break;
-		case 1:
-			suit="Ruter";
-			break;
-		case 2:
-			suit="Klöver";
-			break;
-		case 3:
-			suit="Spader";
-			
-			break;
-		default:
-			break;
-		}
-	}
-	public int getValue() {
-		return value;
-	}
-	public void setValue(int value) {
-		this.value = value;
-	}
-	public int getSuit() {
-		return suit;
-	}
-	public void setSuit(int suit) {
-		this.suit = suit;
-	}
-	@Override
-	public String toString() {
-		return "Card [value=" + value + ", suit=" + suit + "]";
-	}
-	
-	
-	
-	
-	
-
 	
 
 }
--- group4blackjack/src/group4blackjack/Deck.java
+++ group4blackjack/src/group4blackjack/Deck.java
@@ -3,49 +3,11 @@ package group4blackjack;
 import java.util.Random;
 
 public class Deck{
-	/*public static void main(String[] args) { //main print test of the deckOfCards
-		Deck d=new Deck();
-		Card[][] kort=d.cardGenerator();
-		System.out.println(kort);
-	}*/
 	
-
-		Card[][] cardGenerator()
-		{
-			Card deckOfCards[][]=new Card[4][13];
-			for (int i = 0; i < 4; i++) {
-				for (int j = 0; j < 13; j++) {
-					deckOfCards[i][j]=new Card(j,i);
-					
-					
-				}
-				
-			}
-			return deckOfCards;
+	
+	
+	
+	
 		}
-		
-		Card randomCard(Card[][]card)
-		{
-			Card randomCard;
-			int tempRandomCard=0;
-			int tempRandomSuit=0;
-			Random random=new Random();
-			tempRandomCard=random.nextInt(12);
-			tempRandomSuit=random.nextInt(3);
-			randomCard=card[tempRandomSuit][tempRandomCard];
-			
-			
-			
-			return randomCard;
-		}
-		
-		
-	
-	
-	
-	
-	
-
-}
 
 	
