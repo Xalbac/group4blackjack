c9e6753458268b38b72ca0d93941b968ae904afa
nnordling <nnordling@users.noreply.github.com>
2017-10-05 14:14:29 +0200

    player

--- group4blackjack/src/group4blackjack/Player.java
+++ group4blackjack/src/group4blackjack/Player.java
@@ -11,7 +11,17 @@ public class Player {
 	
 	String name;
 
-
+	
+	public int hit() {
+		
+		return 0;
+		
+	}
+	
+	void stay() {
+		System.out.println(name + "stands");
+	}
+	
 	public String getName() {
 		return name;
 	}
