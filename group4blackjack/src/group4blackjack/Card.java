5341a088f17f2b811d66c0c13857dd75c39af250
Svenska Bio <Svenska Bio@SvenskaBio>
2017-10-05 12:51:56 +0200

    new switch in card

--- group4blackjack/src/group4blackjack/Card.java
+++ group4blackjack/src/group4blackjack/Card.java
@@ -5,10 +5,73 @@ public class Card {
 	private int value;
 	private int suit;
 	
-	public Card(int value, int suit) {
+	public Card(int suit, int suit) {
 		super();
-		this.value = value;
+		this.value = suit;
 		this.suit = suit;
+		switch (value) {
+		case 0:
+			value=1;
+			break;
+		case 1:
+			value=2;
+			break;
+		case 2:
+			value=3;
+			break;
+		case 3:
+			suit="Fyra";
+			break;
+		case 4:
+			suit="Fema";
+			break;
+		case 5:
+			suit="Sexa";
+			break;
+		case 6:
+			suit="Sjua";
+			break;
+		case 7:
+			suit="Åtta";
+			break;
+		case 8:
+			suit="Nia";
+			break;
+		case 9:
+			suit="Tia";
+			break;
+		case 10:
+			suit="Knekt";
+			break;
+		case 11:
+			suit="Dam";
+			break;
+		case 12:
+			suit="Kung";
+			break;
+			
+
+		default:
+			break;
+		}
+		
+		switch (suit) {
+		case 0:
+			suit="Hjärter";
+			break;
+		case 1:
+			suit="Ruter";
+			break;
+		case 2:
+			suit="Klöver";
+			break;
+		case 3:
+			suit="Spader";
+			
+			break;
+		default:
+			break;
+		}
 	}
 	public int getValue() {
 		return value;
