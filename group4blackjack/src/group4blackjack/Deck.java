68fd161ccf69cd01ab7754625a79526104d8f7aa
Edvard <Svenska Bio@SvenskaBio>
2017-10-06 13:39:12 +0200

    added shuffler to deck

--- group4blackjack/src/group4blackjack/Deck.java
+++ group4blackjack/src/group4blackjack/Deck.java
@@ -35,7 +35,17 @@ public class Deck {
 	}
 	//shuffle the deck, obviously
 	public void shuffleDeck() {
-		
+	   //new temporary arrayList for shuffled cards
+		ArrayList<Card> tempoDeck= new ArrayList<Card>();
+		Random random =new Random();
+		int indexrandom=0;
+		int originalSizeofDeck=this.cards.size();
+		for (int i = 0; i <originalSizeofDeck ; i++) {
+			indexrandom=random.nextInt((this.cards.size()-1-0)+1)+0;
+			tempoDeck.add(this.cards.get(indexrandom));
+			this.cards.remove(indexrandom);
+		}
+		this.cards=tempoDeck;
 	}
 	
 	
