61f71d5a5dafa7d387ee426f26ae0401b781d0a2
Damian Wozniak <damian.pwozniak@gmail.com>
2017-10-06 11:47:04 +0200

    CARD

--- group4blackjack/src/group4blackjack/Card.java
+++ group4blackjack/src/group4blackjack/Card.java
@@ -5,46 +5,31 @@ import group4blackjack.Deck.Suits;
 
 public class Card {
 
-	private Ranks rank;
-	private Suits suit;
+	private Ranks rank;	// Initialise rank OR VALUE. WHy is it called rank?
+	private Suits suit;	// Initialise suit. 
 
+	// Generate card.
 	public Card(Ranks rank, Suits suit)
 	{
 		this.rank = rank;
 		this.suit = suit;
 	}
 	
+	// So we can see what card it is.
 	public String toString()
 	{
 		return "Card rank= " + rank + " of, suit= " + suit;
 	}
 
-
+	// Return the suit value.
 	public Suits getSuit()
 	{
 		return this.suit;
 	}
 
+	// Return the rank value. 
 	public Ranks getRank()
 	{
 		return this.rank;
 	}
-	/*
-	 * }
-	 * 
-	 * public Ranks getRank() { return rank; }
-	 * 
-	 * public void setRank(Ranks rank) { this.rank = rank; }
-	 * 
-	 * public Suits getSuit() { return suit; }
-	 * 
-	 * public void setSuit(Suits suit) { this.suit = suit; }
-	 */
-	
-	//Some stuff we do not need \/
-
-	/*@Override
-	public boolean equals(Object o) {
-		return (o != null && o instanceof Card && ((Card) o).rank == rank && ((Card) o).suit == suit);
-	}*/
 }
