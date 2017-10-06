bef387a58b79f60dcd33b13d0f269f0fe585299c
Damian Wozniak <damian.pwozniak@gmail.com>
2017-10-06 13:42:05 +0200

    c

--- group4blackjack/src/group4blackjack/Deck.java
+++ group4blackjack/src/group4blackjack/Deck.java
@@ -3,53 +3,60 @@ package group4blackjack;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Deck {
+public class Deck
+{
 	private ArrayList<Card> cards;
 	//private ArrayList<Card> PulledCards;
 
-	public enum Suits {
+	// Enum table. 
+	public enum Suits
+	{
 		SPADES, HEARTS, DIAMONDS, CLUBS;
 	}
 
-	public enum Ranks {
+	// Enum table.
+	public enum Ranks
+	{
 		ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
 	}
+	
+	// Create a deck, 
 	public Deck()
 	{
-		//create new deck
+		// Create new deck.
 		this.cards=new ArrayList<Card>();
 	}
-	//52 cards
-	public void FillDeckWithCards() {
-		//loop through the suits
-	for (Suits cardSuits : Suits.values()) {
-		//loop through the ranks
-		for (Ranks cardRank : Ranks.values()) {
-			//add new card
-			this.cards.add(new Card(cardSuits,cardRank));
-			
+	
+	// 52 cards.
+	public void FillDeckWithCards()
+	{
+		// Loop through the suits. 
+	 	for (Suits cardSuits : Suits.values())
+		{
+			// Loop through the ranks.
+			for (Ranks cardRank : Ranks.values())
+			{
+				// Add new card.
+				this.cards.add(new Card(cardSuits,cardRank));
+				
+			}
 		}
-		
 	}
 	
-	}
-	//shuffle the deck, obviously
-	public void shuffleDeck() {
-	   //new temporary arrayList for shuffled cards
+	// Shuffle the deck, obviously.
+	public void shuffleDeck()
+	{
+		// New temporary arrayList for shuffled cards.
 		ArrayList<Card> tempoDeck= new ArrayList<Card>();
 		Random random =new Random();
 		int indexrandom=0;
 		int originalSizeofDeck=this.cards.size();
-		for (int i = 0; i <originalSizeofDeck ; i++) {
+		for (int i = 0; i <originalSizeofDeck ; i++) 
+		{
 			indexrandom=random.nextInt((this.cards.size()-1-0)+1)+0;
 			tempoDeck.add(this.cards.get(indexrandom));
 			this.cards.remove(indexrandom);
 		}
 		this.cards=tempoDeck;
 	}
-	
-	
-
-
-	
 }
