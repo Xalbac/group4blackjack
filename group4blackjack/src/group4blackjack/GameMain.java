227fd3d2237e96a82c410e55b22a1387181d8a8f
Damian Wozniak <damian.pwozniak@gmail.com>
2017-10-07 10:26:20 +0200

    sad

--- group4blackjack/src/group4blackjack/GameMain.java
+++ group4blackjack/src/group4blackjack/GameMain.java
@@ -7,60 +7,80 @@ public class GameMain
 {
 	public static void main(String[] args)
 	{
+		// Initialise everything. 
 		Deck deckPlay = new Deck();
 		Deck cardsPlayer = new Deck();
 		Deck cardsDealer = new Deck();
 		Player player = new Player();
 		Player dealer = new Player();
 		
+		// Initialise scanner. 
 		Scanner ui = new Scanner(System.in);
 		
+		// Initialise the default values. 
 		int money = 100;
 		boolean playerTurn = true;
 		boolean dealerTurn = false;
 		int bet = 0;
 		
+		// Initialise and shuffle deck. 
 		deckPlay.FillDeckWithCards();
 		deckPlay.shuffleDeck();
 		
+		// Display the welcome message and ask the player for the name.
 		System.out.println("Welcome to the best blackjack ever!\n");
 		System.out.println("What is your name?");
 		String answer = ui.next();
 		player.setName(answer);
 		
-		System.out.println("You have: " + money);
+		// Print out his name and his starting money.
+		System.out.println("Welcome " + player.getName() + "\nYou have: " + money);
 		
+		// As long as the player has money, allow them to play.
 		while (money > 0)
 		{
-			System.out.println("How much would you liek to bet?");
+			// Ask the player about the bet. 
+			System.out.println("How much would you like to bet?");
 			bet = ui.nextInt();
+			
+			// Check if the bet is more or less than the money they currently have. 
 			if (bet > money)
 			{
 				System.out.println("You can't bet more than you have.");
 			}
+			// If the bet is ok, proceed with the game. 
 			else
 			{
+				// Start the game. 
 				System.out.println("Dealing cards.");
 				
+				// Give cards to the player. 
 				cardsPlayer.cardDraw(deckPlay);
 				cardsPlayer.cardDraw(deckPlay);
 				
+				// Give cards to the dealer. 
 				cardsDealer.cardDraw(deckPlay);
 				cardsDealer.cardDraw(deckPlay);
 				
+				// Displayer the cards.
 				System.out.println("Your cards: " + cardsPlayer.cardGet(0).toString() + " " + cardsPlayer.cardGet(1).toString());
-				System.out.println("Your cards are valud at: " + cardsPlayer.cardsValue());
+				System.out.println("Your cards are valued at: " + cardsPlayer.cardsValue());
 				System.out.println("Dealer cards: " + cardsDealer.cardGet(0).toString() + " and 1 hidden.");
-
+				
+				// As long as it's the player's turn...
 				while (playerTurn = true)
 				{
+					// Ask the player if they want to hit or stand.
 					System.out.println("Would you like to [H] hit or [S] stand?");
 					String answer2 = ui.next();
+					
+					// If the answer is hit, do the following. 
 					if (answer2.compareToIgnoreCase("h") == 0)
 					{
 						cardsPlayer.cardDraw(deckPlay);
 						System.out.println("You draw: " + cardsPlayer.cardGet(cardsPlayer.deckSize()-1).toString());
 						
+						// If the player draws a card and the value exceed 21.
 						if (cardsPlayer.cardsValue() > 21)
 						{
 							System.out.println("Busted!");
@@ -68,6 +88,7 @@ public class GameMain
 							playerTurn = false;
 							break;
 						}
+					// If the player stands end the player's turn. 
 					}
 					else if (answer2.compareToIgnoreCase("s") == 0)
 					{
@@ -75,12 +96,14 @@ public class GameMain
 						playerTurn = false;
 						break;
 					}
+					// Else tell htem to use H or S. 
 					else
 					{
 						System.out.println("Please use either [H] or [S]");
 					}
 				}
 				
+				// 
 				if ((cardsDealer.cardsValue() > cardsPlayer.cardsValue()) && playerTurn == false && dealerTurn == true;)
 				{
 					System.out.println("Dealer wins!" + cardsDealer.cardsValue() + " against yours: " + cardsPlayer.cardsValue());
